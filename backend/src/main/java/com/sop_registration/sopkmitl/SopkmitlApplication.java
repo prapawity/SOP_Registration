ce355024c07789ca668e2d53d41914faf8de32d1
prapawity <60070045@kmitl.ac.th>
2019-10-02 16:29:45 +0700

    create backend🏠

---
+++ backend/src/main/java/com/sop_registration/sopkmitl/SopkmitlApplication.java
@@ -0,0 +1,13 @@
+package com.sop_registration.sopkmitl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SopkmitlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SopkmitlApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/sop_registration/sopkmitl/SopkmitlApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sop_registration.sopkmitl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SopkmitlApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
