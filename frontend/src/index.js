8ae52575271c5319b23d0dd0505007372c1c3cd4
prapawity <60070045@kmitl.ac.th>
2019-10-08 12:40:24 +0700

    🎲add material Ui & Router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {Router,Route,Link} from 'react-router'
+import { createBrowserHistory } from "history"
+let browserHistory = createBrowserHistory()
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router history={browserHistory}>
+        {/* <Route exact path="/" component={App}/>
+        <Route path="/test2" component={Test}/>
+        <Route path="/home" component={Navbar}/> */}
+    </Router>,document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
