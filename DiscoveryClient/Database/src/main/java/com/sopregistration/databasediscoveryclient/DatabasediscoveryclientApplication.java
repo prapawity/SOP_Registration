74f978e2b0c7317096d8c02d43c5be6f60ebe19c
prapawity <60070045@kmitl.ac.th>
2019-10-11 16:56:32 +0700

    🏭Create Database DiscoveryClient

---
+++ DiscoveryClient/Database/src/main/java/com/sopregistration/databasediscoveryclient/DatabasediscoveryclientApplication.java
@@ -0,0 +1,13 @@
+package com.sopregistration.databasediscoveryclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabasediscoveryclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabasediscoveryclientApplication.class, args);
+    }
+
+}
