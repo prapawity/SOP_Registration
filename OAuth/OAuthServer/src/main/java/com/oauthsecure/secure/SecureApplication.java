a38b5aac5981e9037ed3b3d2b02c6146942f4e0c
prapawity <protfc16@gmail.com>
2019-11-04 17:25:37 +0700

    🎗test OAuth

--- OAuth/OAuthServer/src/main/java/com/oauthsecure/secure/SecureApplication.java
+++ OAuth/OAuthServer/src/main/java/com/oauthsecure/secure/SecureApplication.java
@@ -2,15 +2,15 @@ package com.oauthsecure.secure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@EnableResourceServer
 @EnableAuthorizationServer
-public class SecureApplication {
+public class SecureApplication  {
 
     public static void main(String[] args) {
         SpringApplication.run(SecureApplication.class, args);
