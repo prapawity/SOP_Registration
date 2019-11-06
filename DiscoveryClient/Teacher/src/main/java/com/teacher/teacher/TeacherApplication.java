5e43f427f27a821fff9a8854565fa39c93c675d8
Supermalonz <60070003@kmitl.ac.th>
2019-11-06 11:31:29 +0700

    add teacher service

--- DiscoveryClient/Teacher/src/main/java/com/teacher/teacher/TeacherApplication.java
+++ DiscoveryClient/Teacher/src/main/java/com/teacher/teacher/TeacherApplication.java
@@ -1,12 +1,25 @@
 package com.teacher.teacher;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @SpringBootApplication
+@EnableEurekaClient
+@RestController
 public class TeacherApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(TeacherApplication.class, args);
     }
 
