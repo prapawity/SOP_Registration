a87548482742ce33bea5f1a32f25e7c3385e30c9
prapawity <protfc16@gmail.com>
2019-11-27 23:23:54 +0700

    config teacher service

--- DiscoveryClient/Teacher/src/main/java/com/teacher/teacher/model/Subject.java
+++ DiscoveryClient/Teacher/src/main/java/com/teacher/teacher/model/Subject.java
@@ -4,10 +4,23 @@ package com.teacher.teacher.model;
 import java.util.List;
 
 public class Subject {
-    private int id;
-    private Demographic demographic;
+    public int id;
+    public List<Subject_Teacher> teacherList;
+    public List<Section> sectionList;
+    public Demographic demographic;
 
-    public Subject(){}
+    //constructor
+    public Subject(){
+
+    }
+
+    public Subject(int id, List<Subject_Teacher> teacherList, List<Section> sectionList, Demographic demographic) {
+        this.id = id;
+        this.teacherList = teacherList;
+        this.sectionList = sectionList;
+        this.demographic = demographic;
+    }
+    //getter setter
 
     public int getId() {
         return id;
@@ -17,6 +30,22 @@ public class Subject {
         this.id = id;
     }
 
+    public List<Subject_Teacher> getTeacherList() {
+        return teacherList;
+    }
+
+    public void setTeacherList(List<Subject_Teacher> teacherList) {
+        this.teacherList = teacherList;
+    }
+
+    public List<Section> getSectionList() {
+        return sectionList;
+    }
+
+    public void setSectionList(List<Section> sectionList) {
+        this.sectionList = sectionList;
+    }
+
     public Demographic getDemographic() {
         return demographic;
     }
@@ -24,5 +53,4 @@ public class Subject {
     public void setDemographic(Demographic demographic) {
         this.demographic = demographic;
     }
-
 }
---
+++ DiscoveryClient/Teacher/src/main/java/com/teacher/teacher/model/Subject_Teacher.java
@@ -0,0 +1,27 @@
+package com.teacher.teacher.model;
+
+public class Subject_Teacher {
+    private int id;
+    private Teacher teacher;
+
+    public Subject_Teacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+    public Subject_Teacher(){}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+}
