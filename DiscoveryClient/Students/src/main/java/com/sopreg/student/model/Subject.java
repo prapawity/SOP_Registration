177b2814480a5d9684c785337a56a58ebc84eb4a
prapawity <protfc16@gmail.com>
2019-11-27 23:37:14 +0700

    config student service

--- DiscoveryClient/Students/src/main/java/com/sopreg/student/model/Subject.java
+++ DiscoveryClient/Students/src/main/java/com/sopreg/student/model/Subject.java
@@ -3,28 +3,53 @@ package com.sopreg.student.model;
 import java.util.List;
 
 public class Subject {
-    private String id;
-    private Demographic demographic;
+    public int id;
+    public List<Subject_Teacher> teacherList;
+    public List<Section> sectionList;
+    public Demographic demographic;
 
-    public Subject(){}
+    //constructor
+    public Subject(){
 
-    public Subject(String id, Demographic demographic) {
-        this.id = id;
-        this.demographic = demographic;
     }
 
-    public String getId() {
+    public Subject(int id, List<Subject_Teacher> teacherList, List<Section> sectionList, Demographic demographic) {
+        this.id = id;
+        this.teacherList = teacherList;
+        this.sectionList = sectionList;
+        this.demographic = demographic;
+    }
+    //getter setter
+
+    public int getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(int id) {
         this.id = id;
+    }
+
+    public List<Subject_Teacher> getTeacherList() {
+        return teacherList;
+    }
+
+    public void setTeacherList(List<Subject_Teacher> teacherList) {
+        this.teacherList = teacherList;
+    }
+
+    public List<Section> getSectionList() {
+        return sectionList;
+    }
+
+    public void setSectionList(List<Section> sectionList) {
+        this.sectionList = sectionList;
     }
 
     public Demographic getDemographic() {
         return demographic;
     }
 
-    public void setDemographic(Demographic demographic) { this.demographic = demographic; }
-
+    public void setDemographic(Demographic demographic) {
+        this.demographic = demographic;
+    }
 }
