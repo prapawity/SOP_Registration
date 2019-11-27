e38f09b1751f8111e74ecc19fcc4884321e3f478
prapawity <protfc16@gmail.com>
2019-11-28 01:14:09 +0700

    fix bugs

--- ServiceGateway/src/main/java/com/sopregistration/servicegateway/ServicegatewayApplication.java
+++ ServiceGateway/src/main/java/com/sopregistration/servicegateway/ServicegatewayApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableDiscoveryClient
